8fbf0c33319060af47d7f03c1fb9ae576bf6019b
YoupiMatos <waitformat@live.fr>
2021-09-18 10:05:11 +0200

    Get_Posts, un peu de CSS

--- app.py
+++ app.py
@@ -5,20 +5,23 @@ from models import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db =SQLAlchemy(app)
+db = SQLAlchemy(app)
+
+def Get_Posts(board):
+    posts = Posts.query.filter(Posts.board == board).all()
+    return posts
 
 @app.route("/")
-def hello_world():
+def Show_Index():
     boards = Boards.query.order_by(Boards.acronym.desc()).all()
     return render_template('index.html', boards = boards)
 
 @app.route("/boards/<string:acronym>")
-def board_view(acronym):
-    return render_template('board.html')
-
-@app.route("/bonjour")
-def bonjour():
-    return "<p>Bonjour.</p>"
+def Board_View(acronym):
+    posts = Get_Posts(acronym)
+    return render_template('board.html', posts = posts)
 
 if __name__ == "__main__":
     app.run(debug = True)
+
+
--- models.py
+++ models.py
@@ -1,17 +1,16 @@
 from app import db
 from datetime import datetime
 
-class  Boards(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+class Boards(db.Model):
+    acronym = db.Column(db.String, nullable = False, unique = True, primary_key = True)
     name = db.Column(db.String, nullable = False)
-    acronym = db.Column(db.String, nullable = False, unique=True)
     description = db.Column(db.String)
 
-# class Threads(db.Model):
-    # pass
-
-class Replies(db.Model):
+class Posts(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String, nullable = False, default="Anonyme")
+    op_id = db.Column(db.Integer)
+    board = db.Column(db.String)
+    subject = db.Column(db.String)
+    name = db.Column(db.String, default="Anonyme")
     date = db.Column(db.DateTime, default = datetime.utcnow)
     content = db.Column(db.String, nullable = False)
