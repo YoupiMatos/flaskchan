a91656eb62c0017ddf9b424b53f0f40051a2d3ed
YoupiMatos <waitformat@live.fr>
2021-09-21 15:50:49 +0200

    Ajout de posts fonctionnel et un peu de CSS Par contre les posts incrémentent les id de 1 peu importe sur quelle planches ils ont été postés, ce qui n'est pas la façon dont un imageboard devrait marcher. Je trouverais une solution un de ces quatres

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy 
 from datetime import datetime
 from models import *
@@ -7,19 +7,41 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+def Get_Boards():
+    boards = Boards.query.order_by(Boards.acronym.asc()).all()
+    return boards
+
 def Get_Posts(board):
     posts = Posts.query.filter(Posts.board == board).all()
     return posts
 
 @app.route("/")
 def Show_Index():
-    boards = Boards.query.order_by(Boards.acronym.desc()).all()
+    boards = Get_Boards()
     return render_template('index.html', boards = boards)
 
-@app.route("/boards/<string:acronym>")
+@app.route("/boards/<string:acronym>", methods = ['POST', 'GET'])
 def Board_View(acronym):
-    posts = Get_Posts(acronym)
-    return render_template('board.html', posts = posts)
+    if request.method == 'POST':
+        subject = request.form['subject']
+        if request.form['name']:
+            name = request.form['name']
+        content = request.form['content']
+        if 'name' in locals():
+            new_post = Posts(op_id = 0, board = acronym, subject = subject, name = name, content = content )
+        else:
+            new_post = Posts(op_id = 0, board = acronym, subject = subject, content = content )
+        
+        try:
+            db.session.add(new_post)
+            db.session.commit()
+            return redirect('/boards/' + acronym)
+        except:
+            return "Erreur lors de l'ajout du post"
+    else:
+        posts = Get_Posts(acronym)
+        boards = Get_Boards()
+        return render_template('board.html', posts = posts, boards = boards, current_board = acronym)
 
 if __name__ == "__main__":
     app.run(debug = True)
